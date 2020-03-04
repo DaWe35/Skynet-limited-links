76ab9b5014985bbf7deddd586678a9d146325eec
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-04 15:38:38 +0100

    add ip_ban table

--- sia_links.sql
+++ sia_links.sql
@@ -3,6 +3,12 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE `ip_ban` (
+  `id` int(11) NOT NULL,
+  `ip` int(11) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 CREATE TABLE `sia_links` (
   `id` varchar(12) COLLATE utf8_bin NOT NULL,
   `skylink` varchar(46) COLLATE utf8_bin NOT NULL,
@@ -11,7 +17,14 @@ CREATE TABLE `sia_links` (
   `password` varchar(63) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+ALTER TABLE `ip_ban`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `sia_links`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
+
+
+ALTER TABLE `ip_ban`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 COMMIT;
